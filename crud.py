3891a305f8f25fb9dabe57dc0071f37f2fa76717
Molly Riley <mo.riley@gmail.com>
2021-10-28 21:22:50 +0000

    finished step 3

--- crud.py
+++ crud.py
@@ -16,6 +16,17 @@ def create_user(email, password):
 
     return user
 
+def get_users():
+    """Return a users."""
+
+    return User.query.all()
+
+
+def get_user_by_id(user_id):
+    """get each user for user webpages"""
+
+    return User.query.get(user_id)
+
 
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
@@ -25,6 +36,17 @@ def create_movie(title, overview, release_date, poster_path):
     db.session.commit()
 
     return movie
+
+def get_movies(): 
+    """Return all movies"""
+
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    """get each move for movie webpages"""
+
+    return Movie.query.get(movie_id)
+
 
 def create_rating(user, movie, score):
     """Create and return a new rating."""
--- server.py
+++ server.py
@@ -1,14 +1,55 @@
 """Server for movie ratings app."""
 
-from flask import Flask
+from flask import (Flask, render_template, request, flash, session,
+    redirect)
+
+from model import connect_to_db
+import crud
+
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined 
 
+@app.route('/')
+def homepage():
+    """View homepage."""
+    return render_template('homepage.html')
 
-# Replace this with routes and view functions!
+@app.route('/movies')
+def all_movies():
+    """view all movies"""
+
+    movies = crud.get_movies()
+
+    return render_template("all_movies.html", movies=movies)
+
+@app.route("/movies/<movie_id>")
+def show_movie(movie_id):
+    """ Show details on a particular movie. """
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template("movie_details.html", movie=movie)
+
+@app.route('/users')
+def all_users():
+    """view all user"""
+
+    users = crud.get_users()
+
+    return render_template("all_users.html", users=users)
+
+@app.route("/users/<user_id>")
+def show_user(user_id):
+    """ Show particular user page. """
+    user = crud.get_user_by_id(user_id)
+
+    return render_template("user_details.html", user=user)
+
 
 
 if __name__ == "__main__":
-    # DebugToolbarExtension(app)
-
+    #DebugToolbarExtension(app)
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
